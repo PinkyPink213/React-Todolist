13985663168ffd707a0477d9879a6b0bf0883672
PinkPronto <52145683+PinkPronto@users.noreply.github.com>
2020-05-31 21:34:34 +0700

    Update Functional Component

--- src/App.js
+++ src/App.js
@@ -1,85 +1,68 @@
-import React,{ Component } from 'react';
+import React,{ useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { v4 as uuidv4 } from 'uuid';
 import TodoInput from './component/TodoInput';
 import TodoList from './component/TodoList';
-class App extends Component {
-  state={
-    items: [
-      { id: 1, title: "wake up" },
-      { id: 2, title: "breakfast" },
-    ],
-    id: uuidv4(),
-    item: '',
-    editItem: false
+const App=()=> {
+  const [items,setItems] = useState([]);
+  const [id,setId] = useState(uuidv4());
+  const [item, setItem] = useState('');
+  const [editItem,setEditItem] = useState(false);
+
+  const handleChange = e => {
+    setItem(e.target.value);
   };
-  handleChange = e => {
-    this.setState({
-      item: e.target.value
-    });
-  };
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     const newItem = {
-      id: this.state.id,
-      title: this.state.item
-    };
-    const updatedItems = [...this.state.items, newItem];
-
-    this.setState({
-      items: updatedItems,
-      item: "",
-      id: uuidv4(),
-      editItem: false
-    });
-  };
-  clearList = () => {
-    this.setState({
-      items: []
-    });
-  };
-  handleDelete = id => {
-    const filteredItems = this.state.items.filter(item => item.id !== id);
-    this.setState({
-      items: filteredItems
-    });
-  };
-  handleEdit = id => {
-    const filteredItems = this.state.items.filter(item => item.id !== id);
-    const selectedItem = this.state.items.find(item => item.id === id);
-    this.setState({
-      items: filteredItems,
-      item: selectedItem.title,
       id: id,
-      editItem: true
-    });
+      title: item
+    };
+    const updatedItems = [...items, newItem];
+    setItems(updatedItems);
+    setItem("");
+    setId(uuidv4());
+    setEditItem(false);
   };
-  render(){
-    console.log( this.state);
-      return (
-          <div className="container">
-            <div className="row">
-              <div className="col-10 mx-auto col-md-8 mt-5">
-                <h3 className="text-capitalize text-center">todo input</h3>
-                <TodoInput
-                  item={this.state.item}
-                  handleChange={this.handleChange}
-                  handleSubmit={this.handleSubmit}
-                  editItem={this.state.editItem}
-                  />
-                  <TodoList
-                    items={this.state.items}
-                    clearList={this.clearList}
-                    handleDelete={this.handleDelete}
-                    handleEdit={this.handleEdit}
-                  />
-              </div>
-            </div>
-          </div>
-      );
-    }
-  }
+  const clearList = () => {
+    setItems([]);
+  };
+  const handleDelete = id => {
+    const filteredItems =  items.filter(item => item.id !== id);
+    setItems( filteredItems);
+  };
+  const handleEdit = id => {
+    const filteredItems = items.filter(item => item.id !== id);
+    const selectedItem = items.find(item => item.id === id);
+    setItems(filteredItems);
+    setItem(selectedItem.title);
+    setId(id);
+    setEditItem(true);
+  };
 
+  return (
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={item}
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              editItem={editItem}
+              />
+              <TodoList
+                items={items}
+                clearList={clearList}
+                handleDelete={handleDelete}
+                handleEdit={handleEdit}
+              />
+          </div>
+        </div>
+      </div>
+  );
+
+}
 
 export default App;
